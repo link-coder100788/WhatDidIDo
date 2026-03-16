23b697c76b3add90c3ceaa52e8a9ec84b39381bb
link-coder100788 <240818174+link-coder100788@users.noreply.github.com>
2026-03-16 14:16:17 -0700

    Verison refresh to 1.4.0

--- Sources/WhatDidIDo/WhatDidIDoCore.swift
+++ Sources/WhatDidIDo/WhatDidIDoCore.swift
@@ -3,7 +3,7 @@ import Foundation
 enum Info {
 	static let owner = "link-coder100788"
 	static let repo = "WhatDidIDo"
-	static let currentVersion = "1.3.9"
+	static let currentVersion = "1.4.0"
 }
 
 enum Shell {

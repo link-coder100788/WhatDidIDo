fb795f56e63ec4d4f501d0e0b9d3f53f9d7aaddf
link-coder100788 <240818174+link-coder100788@users.noreply.github.com>
2026-03-16 09:03:28 -0700

    v1.3.5

--- Sources/WhatDidIDo/WhatDidIDoCore.swift
+++ Sources/WhatDidIDo/WhatDidIDoCore.swift
@@ -3,7 +3,7 @@ import Foundation
 enum Info {
 	static let owner = "link-coder100788"
 	static let repo = "WhatDidIDo"
-	static let currentVersion = "1.3.4"
+	static let currentVersion = "1.3.5"
 }
 
 enum Shell {
